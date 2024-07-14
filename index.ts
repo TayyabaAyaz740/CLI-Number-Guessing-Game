802dde6282c6ec23e55934be8154be8821099c69
Tayyaba Ayaz <161968349+TayyabaAyaz740@users.noreply.github.com>
2024-07-14 10:24:03 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! usr/bin/env node
+import { log } from "console";
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to Code with Tayyaba CLI- Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+{
+    name : "userGuessedNumber",
+    type: "number",
+    message: "Enter your guess number(Number limit from 1 to 5):",
+},
+]);
+
+if(answer.userGuessedNumber === randomNumber){
+console.log(" Congratulation! You guess a right number.");
+}
+else{
+    console.log("Sorry, you guess a wrong number.")
+}
